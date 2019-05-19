ccd6317b9f44e64928a011924393e13ee5a8bf01
Jacek Wesołowski <jacqu25@yahoo.com>
2019-05-19 11:52:13 +0200

    file deleted

--- autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require __DIR__.'/vendor/autoload.php';
-
-$configuration =  array(
-    'db_dsn' => 'mysql:host=localhost;dbname=worldCup_db;charset=utf8',
-    'db_user' => 'root',
-    'db_pass' => 'coderslab'
-);
